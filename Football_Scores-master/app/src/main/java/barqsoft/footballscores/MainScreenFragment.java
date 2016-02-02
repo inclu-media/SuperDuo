87183c656310dc0dd029464ed89ead206b597b49
Martin Melcher <martin@inclumedia.at>
2016-02-02 12:27:22 +0100

    Wikimedia thumb generator

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/MainScreenFragment.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/MainScreenFragment.java
@@ -13,8 +13,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import com.bumptech.glide.Glide;
-
 import barqsoft.footballscores.service.myFetchService;
 
 /**
@@ -59,7 +57,7 @@ public class MainScreenFragment extends Fragment implements LoaderManager.Loader
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
             {
-                ViewHolder selected = (ViewHolder) view.getTag();
+                scoresAdapter.ViewHolder selected = (scoresAdapter.ViewHolder) view.getTag();
                 mAdapter.detail_match_id = selected.match_id;
                 MainActivity.selected_match_id = (int) selected.match_id;
                 mAdapter.notifyDataSetChanged();
--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
@@ -1,13 +1,8 @@
 package barqsoft.footballscores;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Picture;
-import android.graphics.drawable.PictureDrawable;
-import android.net.Uri;
-import android.os.Build;
 import android.support.v4.widget.CursorAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,26 +12,12 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.GenericRequestBuilder;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.ResourceDecoder;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.engine.Resource;
-import com.bumptech.glide.load.model.StreamEncoder;
-import com.bumptech.glide.load.resource.SimpleResource;
-import com.bumptech.glide.load.resource.file.FileToStreamDecoder;
-import com.bumptech.glide.load.resource.transcode.ResourceTranscoder;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.ImageViewTarget;
-import com.bumptech.glide.request.target.Target;
-import com.caverock.androidsvg.SVG;
-import com.caverock.androidsvg.SVGParseException;
+import com.squareup.picasso.Picasso;
 
-import org.joda.time.DateTime;
-import org.joda.time.LocalDate;
+import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * Created by yehya khaled on 2/26/2015.
@@ -45,13 +26,13 @@ import java.io.InputStream;
  * - Changed share functionality to use chooser and got rid of "sunshine" code
  * - Made sharing hashtag a string resource
  * - Fixed League display (old league codes were used)
- * - Use glide for displaying crests-svg's (url was previously loaded into db during fetch)
+ * - Use wikimedia thumbnail generator for crests
  */
 public class scoresAdapter extends CursorAdapter
 {
     private static final String LOG_TAG = scoresAdapter.class.getSimpleName();
-    private GenericRequestBuilder<Uri, InputStream, SVG, PictureDrawable> mSVGRequestBuilder;
     private Context mContext;
+    private static Pattern urlPatter = Pattern.compile("/(\\w{2}/)(\\w{1}/\\w{2}/)(.*$)");
 
     public static final int COL_DATE = 1;
     public static final int COL_MATCHTIME = 2;
@@ -67,86 +48,54 @@ public class scoresAdapter extends CursorAdapter
 
     public double detail_match_id = 0;
 
-    private class SvgDecoder implements ResourceDecoder<InputStream, SVG> {
-        public Resource<SVG> decode(InputStream source, int width, int height) throws IOException {
-            try {
-                SVG svg = SVG.getFromInputStream(source);
-                return new SimpleResource<SVG>(svg);
-            } catch (SVGParseException ex) {
-                throw new IOException("Cannot load SVG from stream", ex);
-            }
-        }
+    public static class ViewHolder
+    {
+        public TextView home_name;
+        public TextView away_name;
+        public TextView score;
+        public TextView date;
+        public ImageView home_crest;
+        public ImageView away_crest;
+        public double match_id;
 
-        @Override
-        public String getId() {
-            return "SvgDecoder.com.bumptech.svgsample.app";
-        }
-    }
-
-    private class SvgDrawableTranscoder implements ResourceTranscoder<SVG, PictureDrawable> {
-        @Override
-        public Resource<PictureDrawable> transcode(Resource<SVG> toTranscode) {
-            SVG svg = toTranscode.get();
-
-            int dimen = Math.round(Utilies.dipToPixels(mContext, 48));
-            svg.setDocumentHeight(dimen);
-            svg.setDocumentWidth(dimen);
-
-            Picture picture = svg.renderToPicture(dimen, dimen);
-
-            PictureDrawable drawable = new PictureDrawable(picture);
-            return new SimpleResource<PictureDrawable>(drawable);
-        }
-
-        @Override
-        public String getId() {
-            return "";
+        public ViewHolder(View view)
+        {
+            home_name = (TextView) view.findViewById(R.id.home_name);
+            away_name = (TextView) view.findViewById(R.id.away_name);
+            score     = (TextView) view.findViewById(R.id.score_textview);
+            date      = (TextView) view.findViewById(R.id.data_textview);
+            home_crest = (ImageView) view.findViewById(R.id.home_crest);
+            away_crest = (ImageView) view.findViewById(R.id.away_crest);
         }
     }
 
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private class SvgSoftwareLayerSetter<T> implements RequestListener<T, PictureDrawable> {
+    private void loadCrest(String crestUrl, ImageView view) {
 
-        @Override
-        public boolean onException(Exception e, T model, Target<PictureDrawable> target, boolean isFirstResource) {
-            ImageView view = ((ImageViewTarget<?>) target).getView();
-            if (Build.VERSION_CODES.HONEYCOMB <= Build.VERSION.SDK_INT) {
-                view.setLayerType(ImageView.LAYER_TYPE_NONE, null);
-            }
-            return false;
-        }
-
-        @Override
-        public boolean onResourceReady(PictureDrawable resource, T model, Target<PictureDrawable> target,
-                                       boolean isFromMemoryCache, boolean isFirstResource) {
-            ImageView view = ((ImageViewTarget<?>) target).getView();
-            if (Build.VERSION_CODES.HONEYCOMB <= Build.VERSION.SDK_INT) {
-                view.setLayerType(ImageView.LAYER_TYPE_SOFTWARE, null);
-            }
-
-            return false;
-        }
-    }
-
-
-    private void loadCrest(String crestUrl, ImageView view, boolean showCrest) {
-
-        // don't get the crest from SCG for past matches
-        // saves memory
-        if (!showCrest) {
+        if (crestUrl.isEmpty()) {
             view.setImageResource(R.drawable.crest_48);
             return;
         }
 
-        if (crestUrl.endsWith(".png")) {
-            int dimen = Math.round(Utilies.dipToPixels(mContext, 48));
-            Glide.with(mContext).load(Uri.parse(crestUrl)).override(dimen, dimen)
-                    .centerCrop().into(view);
+        int dimen = Math.round(Utilies.dipToPixels(mContext, 48));
+
+        if (crestUrl.endsWith(".svg")) {
+            Matcher matcher = urlPatter.matcher(crestUrl);
+            if (matcher.find()) {
+                String lang = matcher.group(1);
+                String path = matcher.group(2);
+                String fileName = matcher.group(3);
+                crestUrl =  mContext.getString(R.string.wikimedia_thumb_url) +
+                        lang + "thumb/" +
+                        path + fileName +
+                        "/" + dimen + "px-" + fileName + ".png";
+            }
+            else {
+                view.setImageResource(R.drawable.crest_48);
+                return;
+            }
         }
-        else {
-            mSVGRequestBuilder.diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                    .load(Uri.parse(crestUrl)).into(view);
-        }
+        Picasso.with(mContext).load(crestUrl).resize(dimen, dimen)
+                .placeholder(R.drawable.crest_48).error(R.drawable.crest_48).centerCrop().into(view);
     }
 
     public scoresAdapter(Context context,Cursor cursor,int flags)
@@ -154,17 +103,6 @@ public class scoresAdapter extends CursorAdapter
         super(context,cursor,flags);
 
         mContext = context;
-        mSVGRequestBuilder = Glide.with(context)
-                .using(Glide.buildStreamModelLoader(Uri.class, context), InputStream.class)
-                .from(Uri.class)
-                .as(SVG.class)
-                .transcode(new SvgDrawableTranscoder(), PictureDrawable.class)
-                .sourceEncoder(new StreamEncoder())
-                .cacheDecoder(new FileToStreamDecoder<SVG>(new SvgDecoder()))
-                .decoder(new SvgDecoder())
-                .placeholder(R.drawable.crest_48)
-                .error(R.drawable.crest_48)
-                .listener(new SvgSoftwareLayerSetter<Uri>());
     }
 
     @Override
@@ -191,15 +129,10 @@ public class scoresAdapter extends CursorAdapter
         holder.score.setText(score);
         holder.match_id = cursor.getDouble(COL_ID);
 
-        // set crests using Glide
-        // only render SVG crests for today's matches (high memory consumption)
-        DateTime dt = new DateTime(cursor.getString(COL_DATE));
-        boolean useCrests = ((dt.toLocalDate()).equals(new LocalDate()));
-
         String homeCrestUrl = cursor.getString(COL_HOME_URL);
-        loadCrest(homeCrestUrl, holder.home_crest, useCrests);
+        loadCrest(homeCrestUrl, holder.home_crest);
         String awayCrestUrl = cursor.getString(COL_AWAY_URL);
-        loadCrest(awayCrestUrl, holder.away_crest, useCrests);
+        loadCrest(awayCrestUrl, holder.away_crest);
 
         LayoutInflater vi = (LayoutInflater) context.getApplicationContext()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
